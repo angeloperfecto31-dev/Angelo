9d448a122d0ae881d29e0f7b96dc236458affb02
Angelo Perfecto <angeloperfecto31@gmail.com>
2026-07-09 21:37:05 +0800

    refactor: remove manual patching scripts
    Clean up the repository by removing legacy patch scripts used for iterative updates to compute logic and diagram strings.

--- patch_word.js
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-let content = fs.readFileSync('src/utils/exportWord.ts', 'utf8');
-
-content = content.replace(/\\\$\\{cir\.mcbP \|\| 2\\}P/g, '${typeof cir.mcbP === "string" ? cir.mcbP : (cir.mcbP || 2) + "P"}');
-content = content.replace(/\\\$\\{cir\.mcbP\\}P/g, '${typeof cir.mcbP === "string" ? cir.mcbP : cir.mcbP + "P"}');
-
-fs.writeFileSync('src/utils/exportWord.ts', content, 'utf8');
